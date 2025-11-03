708f0867d71f47915c8c2e8f499acfa7b5093d61
CapitanCrunch <ramsuzumaki@gmail.com>
2025-11-03 02:25:53 -0600

    MR: Definí la clase analyzer (Izuy)

--- medir_rendimiento.py
+++ medir_rendimiento.py
@@ -5,5 +5,25 @@ import matplotlib.pyplot as plt
 from FA import OptimizedAdaptiveFilter, ParallelPipelineDetector
 from baseline_secuencial import SequentialBaseline
 import json
+from multiprocessing import Process, Queue, Value 
+from queue import Empty 
+import os
 
 class PerformanceAnalyzer:
+    def __init__(self, num_frames=100):
+        self.num_frames = num_frames
+        self.results = {
+            'sequential': {
+                'times': [],
+                'avg_time': 0,
+                'fps': 0
+            },
+            'parallel': {
+                'times': [],
+                'avg_time': 0,
+                'fps': 0
+            },
+            'speedup': 0,
+            'efficiency': 0,
+            'num_threads': 2  #Capturar y procesar
+        }
