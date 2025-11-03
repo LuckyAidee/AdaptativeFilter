740fbdf5a817e1d2d9d806760f8d14db9b55f879
CapitanCrunch <ramsuzumaki@gmail.com>
2025-11-03 01:29:02 -0600

    MR: Cree la clase analyzer (Izuy)

--- medir_rendimiento.py
+++ medir_rendimiento.py
@@ -1,1 +1,9 @@
 import cv2
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from FA import OptimizedAdaptiveFilter, ParallelPipelineDetector
+from baseline_secuencial import SequentialBaseline
+import json
+
+class PerformanceAnalyzer:
